40fa30c4253413ccc21d81ec7a8f22721beb8163
Artur Sterz <sterz@trackit.systems>
2025-11-17 10:46:27 +0100

    Use own spectrogram re-implementation

--- back/src/sonari/routes/spectrograms.py
+++ back/src/sonari/routes/spectrograms.py
@@ -50,7 +50,6 @@ async def get_spectrogram(
         Spectrogram image.
 
     """
-    print(f"{start_time}" + " " + f"{end_time}" + " " + f"{audio_parameters}" + " " + f"{spectrogram_parameters}")
     recording = await api.recordings.get(session, recording_id)
 
     # Close session BEFORE expensive computation
