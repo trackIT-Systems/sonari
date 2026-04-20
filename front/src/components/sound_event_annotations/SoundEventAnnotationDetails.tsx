dc4a797bef13c75268fa0abf3cc995b06dc581a8
melinamorch <melina.morch@gmail.com>
2026-04-20 13:47:09 +0200

    display species vs detection confidence

--- front/src/components/sound_event_annotations/SoundEventAnnotationDetails.tsx
+++ front/src/components/sound_event_annotations/SoundEventAnnotationDetails.tsx
@@ -1,4 +1,3 @@
-import Card from "@/components/Card";
 import { DescriptionData, DescriptionTerm } from "@/components/Description";
 import { H4 } from "@/components/Headings";
 
@@ -9,6 +8,11 @@ export default function SoundEventAnnotationDetails({
 }: {
   soundEventAnnotation: SoundEventAnnotation;
 }) {
+  const creatorUsername = soundEventAnnotation.created_by?.username;
+  const useSpeciesConfidence = creatorUsername == "yolobat";
+  const confidenceFeatureName = useSpeciesConfidence
+    ? "species_confidence"
+    : "detection_confidence";
   return (
     <div className="flex flex-col gap-2">
       <div className="flex justify-between items-center gap-2 mb-2">
@@ -17,12 +21,14 @@ export default function SoundEventAnnotationDetails({
         </H4>
       </div>
       <div className="flex flex-row flex-wrap gap-4">
-        {soundEventAnnotation.features?.filter(feature => feature.name === "detection_confidence").map((feature) => (
-          <div key={feature.name}>
-            <DescriptionTerm>Confidence</DescriptionTerm>
-            <DescriptionData>{feature.value.toLocaleString()}</DescriptionData>
-          </div>
-        ))}
+        {soundEventAnnotation.features
+          ?.filter((feature) => feature.name === confidenceFeatureName)
+          .map((feature) => (
+            <div key={feature.name}>
+              <DescriptionTerm>Confidence</DescriptionTerm>
+              <DescriptionData>{feature.value.toLocaleString()}</DescriptionData>
+            </div>
+          ))}
         {soundEventAnnotation.created_by && (
           <div>
             <DescriptionTerm>Created by</DescriptionTerm>
